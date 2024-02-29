0815b7a3d3ed92e4d39cbe4602564a06046583bd
Piano-player25 <133797848+Piano-player25@users.noreply.github.com>
2024-02-29 16:24:26 +0100

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,380 @@
+const d = {
+	"list": [
+		/*=================================================================================
+		{
+			"vids": [
+				{
+					"user": "name here",
+					"link": "yt link here",
+					"percent": "remove these quotations and replace it with an integer (numbers)",
+					"hz": "refresh rate here (Make sure you add Hz at the end"
+				},
+
+      ],
+			"name": "Level name here.",
+			"author": "Author name here",
+			"more": "none",
+			"id": "remove quotations and replace them with the level ID (integers)",
+			"pass": "Level pass here",
+			"percentToQualify": "remove quotations and replace them with integers (numbers)",
+			"verificationVid": "verification vid link here (youtube and twitch only allowed)",
+			
+/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*\/
+			"key": 0
+		},
+		=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Congregeist",
+			"author": "Username [Potato]",
+			"more": "none",
+			"id": 128,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=VgCy6FoWEF0",
+			"key": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Lunar Abyss",
+			"author": "DaEchteEvo",
+			"more": "none",
+			"id": 101,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 1
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Interstellar",
+			"author": "Username",
+			"more": "none",
+			"id": 101,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=FY6uo0FhVlQ",
+			"key": 2
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "User",
+					"link": "https://www.youtube.com/watch?v=3sJM5gyI848",
+					"percent": 100,
+					"hz": "60 Hz"
+				},
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "OuterSpace",
+			"author": "Username",
+			"more": "none",
+			"id": 171,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 3
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "Chameleon",
+			"author": "Username",
+			"more": "none",
+			"id": 248,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 4
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "White",
+			"author": "Username",
+			"more": "none",
+			"id": 298,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 5
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Spike Dungeon",
+			"author": "Maskedflame443",
+			"more": "none",
+			"id": 379,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 6
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "Grecongation",
+			"author": "ClooVlieg",
+			"more": "none",
+			"id": 144,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 7
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Le Troisieme Pole",
+			"author": "ILikeBaguette",
+			"more": "none",
+			"id": 375,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=urmoyZcbL-k",
+			"key": 8
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "Need You",
+			"author": "User",
+			"more": "none",
+			"id": 231,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 9
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Crepusculo",
+			"author": "Maskedflame443",
+			"more": "none",
+			"id": 374,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 10
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Color Fusion",
+			"author": "ILikeBaguette",
+			"more": "none",
+			"id": 261,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=rphB4TGC7_o",
+			"key": 11
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "Crystallize",
+			"author": "ClooVlieg",
+			"more": "none",
+			"id": 121,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 12
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "No Way Out",
+			"author": "Username",
+			"more": "none",
+			"id": 67,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 13
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Depression Conical",
+			"author": "Username",
+			"more": "none",
+			"id": 308,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 14
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "Dont Let Go",
+			"author": "Username",
+			"more": "none",
+			"id": 195,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 15
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "Forest Fire",
+			"author": "Username",
+			"more": "none",
+			"id": 26,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 16
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Cutterflunk 2point2",
+			"author": "Maskedflame443",
+			"more": "none",
+			"id": 385,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 17
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "ILikeBaguette",
+					"link": "https://www.youtube.com/watch?v=KSCPULlJTQQ",
+					"percent": 100,
+					"hz": "144 Hz"
+				},
+
+      ],
+			"name": "GemAdventure",
+			"author": "LenoxAYE",
+			"more": "none",
+			"id": 180,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 18
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "G Bit",
+			"author": "Maskedflame443",
+			"more": "none",
+			"id": 386,
+			"pass": "N/A",
+			"percentToQualify": 100,
+			"verificationVid": "",
+			"key": 19
+		},
+		/*=================================================================================*/
+
+
+
+
+/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
